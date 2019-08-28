280323012ac57424d725d587f4d2bb66d0acf928
mille424 <mille424@gmail.com>
2019-08-27 17:24:29 -0700

    removed some old comments

--- server/index.js
+++ server/index.js
@@ -29,20 +29,10 @@ app.get('/comments/:artist/:song', (req, res) => {
   console.log(`GET request for artist ${req.params.artist} and song ${req.params.song}`)
   //make sure server for port 3002 is running
   request(`http://localhost:3002/comments/${req.params.artist}/${req.params.song}`).pipe(res);
-  //   request(`http://localhost:3002/comments/${req.params.artist}/${req.params.song}`, (request, response, body) => {
-  //     if(body) {
-  //       res.set({
-  //         'content-type': 'application/json'
-  //       })
-  //       res.send(JSON.parse(body));
-  //     } else {
-  //       res.end();
-  //     }
-  //   })
 })
 
 
-const port = 3333
+const port = 3000
 app.listen(port, () => {
   console.log('server is listening on port', port);
 })
